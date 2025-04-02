51443d79950792ce8a6917f8a2d9fd8b63af6b2a
Victorleven <victor.leven@elev.ga.ntig.se>
2025-04-02 22:41:37 +0200

    Worked on projects, footer and mobile layout
    Fixed the mobile layout including the completed footer and designed the projects page

--- js/index.js
+++ js/index.js
@@ -12,16 +12,22 @@ function imageSlide(slideNumber) {
         document.getElementById("heroimg").style.backgroundImage = "url(/img/r-architecture-rOk4VSMS3Ck-unsplash.jpg)"
     } 
     if (slideNumber == 2) {
-        document.getElementById("heroimg").style.backgroundImage = "url(/img/r-architecture-2gDwlIim3Uw-unsplash.jpg)"
+        document.getElementById("heroimg").style.backgroundImage = "url(/img/vidar-nordli-mathisen-q-fr4QsIyic-unsplash.jpg)"
+        //heroImg.style.backgroundImage = "url(/img/r-architecture-2gDwlIim3Uw-unsplash.jpg)"
+    }
+    if (slideNumber == 3) {
+        document.getElementById("heroimg").style.backgroundImage = "url(/img/r-architecture-khpWE85ge38-unsplash.jpg)"
         //heroImg.style.backgroundImage = "url(/img/r-architecture-2gDwlIim3Uw-unsplash.jpg)"
     }
 }
 
 const firstButton = document.querySelector("#firstButton")
 const secondButton = document.querySelector("#secondButton")
+const thirdButton = document.querySelector("#thirdButton")
 
 firstButton.addEventListener("click", () => imageSlide(1))
 secondButton.addEventListener("click", () => imageSlide(2))
+thirdButton.addEventListener("click", () => imageSlide(3))
 
 
     //document.getElementById("firstButton").addEventlistener("click", () => imageSlide(1))
