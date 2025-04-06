dfcccc6d79961288dec184b25e58d7d07a1f414c
Victorleven <victor.leven@elev.ga.ntig.se>
2025-04-06 22:08:20 +0200

    Completed css and html
    Some javascript remaining

--- js/index.js
+++ js/index.js
@@ -24,11 +24,16 @@ function imageSlide(slideNumber) {
 const firstButton = document.querySelector("#firstButton")
 const secondButton = document.querySelector("#secondButton")
 const thirdButton = document.querySelector("#thirdButton")
+const logo = document.querySelector("#logo")
 
 firstButton.addEventListener("click", () => imageSlide(1))
 secondButton.addEventListener("click", () => imageSlide(2))
 thirdButton.addEventListener("click", () => imageSlide(3))
+logo.addEventListener("click", () => firstPage())
 
+function firstPage() {
+    window.location.assign("http://127.0.0.1:5500/index.html")
+}
 
     //document.getElementById("firstButton").addEventlistener("click", () => imageSlide(1))
     //document.getElementById("secondButton").addEventlistener("click", () => imageSlide(2))
